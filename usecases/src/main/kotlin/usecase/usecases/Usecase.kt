ea015882328644a108f9ecd62352e2a9534e00cb
KIM JUNG BIN <kjbin0420@naver.com>
2021-10-24 17:06:16 +0900

    ⚡️ :: 관리자 인증 로직 추가 (#12)

--- usecases/src/main/kotlin/usecase/usecases/Usecase.kt
+++ usecases/src/main/kotlin/usecase/usecases/Usecase.kt
@@ -1,16 +1,27 @@
 package usecase.usecases
 
+import domain.PasswordNotMatchedException
+
 /**
  * @author smoothbear
  * Can not be extended at outside of module.
  */
 sealed class UsecaseType {
-    /**
-     * authentication logics..
-     */
+    fun auth(password: String) {
+        if (!System.getenv("ADMIN_PASSWORD").equals(password))
+            throw PasswordNotMatchedException()
+    }
 }
 
-abstract class Usecase<T: Any, R: Any> : UsecaseType(), suspend (T) -> R {
+abstract class NormalUsecase<T: Any, R: Any> : UsecaseType(), suspend (T) -> R {
     protected abstract suspend fun executor(request: T): R
     override suspend fun invoke(request: T): R = executor(request)
+}
+
+abstract class AuthUsecase<T: Any, R: Any> : UsecaseType(), suspend (T, String) -> R {
+    protected abstract suspend fun executor(request: T): R
+    override suspend fun invoke(request: T, password: String): R {
+        auth(password)
+        return executor(request)
+    }
 }
