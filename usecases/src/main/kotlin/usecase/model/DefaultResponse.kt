f75470363d95ff8ef5044a795b7aa0f7e70bd3a1
smoothbear <kjbin0420@naver.com>
2021-12-24 11:45:07 +0900

    Add response

--- usecases/src/main/kotlin/usecase/model/DefaultResponse.kt
+++ usecases/src/main/kotlin/usecase/model/DefaultResponse.kt
@@ -1,5 +1,13 @@
 package usecase.model
 
+import domain.entity.ResourceType
+
 open class DefaultResponse (
     val message: String
+)
+
+open class Response (
+    val name: String,
+    val url: String,
+    val type: ResourceType
 )
--- usecases/src/main/kotlin/usecase/usecases/logo/UploadLogoUsecase.kt
+++ usecases/src/main/kotlin/usecase/usecases/logo/UploadLogoUsecase.kt
@@ -6,6 +6,7 @@ import domain.entity.ResourceType
 import domain.repository.ResourceRepository
 import usecase.Environment
 import usecase.model.DefaultResponse
+import usecase.model.Response
 import usecase.model.UploadLogoRequest
 import usecase.usecases.AuthUsecase
 import usecase.usecases.common.writeFile
@@ -14,8 +15,8 @@ import java.io.File
 class UploadLogoUsecase (
     private val repository: ResourceRepository,
     private val environment: Environment
-) : AuthUsecase<UploadLogoRequest, DefaultResponse>() {
-    override suspend fun executor(request: UploadLogoRequest): DefaultResponse {
+) : AuthUsecase<UploadLogoRequest, Response>() {
+    override suspend fun executor(request: UploadLogoRequest): Response {
 
         val file = request.file
         val logoName = request.logoName
@@ -37,6 +38,10 @@ class UploadLogoUsecase (
             )
         )
 
-        return DefaultResponse("로고가 성공적으로 등록되었습니다.")
+        return Response(
+            name = logoName,
+            url = "${environment.baseUrl}/logo/$logoName.$ext",
+            type = ResourceType.LOGO
+        )
     }
 }
